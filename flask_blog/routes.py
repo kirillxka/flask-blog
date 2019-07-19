75e5e4d6deb0c54284fede6d0b20f4ba46e87fe8
Kirill <xaritonov.xka-95@mail.ru>
2019-07-19 23:20:01 +0300

    Change project structure to Package

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'KEY'
+from flask import render_template, url_for, flash, redirect
+from flask_blog import app
+from flask_blog.forms import RegistrationForm, LoginForm
+from flask_blog.models import User, Post
 
 @app.route('/')
 @app.route('/home')
@@ -25,6 +24,3 @@ def register():
 def login():
     form = LoginForm()
     return render_template('login.html', title='Login', form=form)
-
-if __name__ == '__main__':
-    app.run(debug=True)
