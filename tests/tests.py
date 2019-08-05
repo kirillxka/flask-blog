bcca64a4c8a7c8dbc63f72dd516a8f4b96cd0e35
Kirill <xaritonov.xka-95@mail.ru>
2019-08-05 16:17:16 +0300

    Add basic test

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,29 @@
 import unittest
-from flask_blog import create_app
-from flask_blog.models import User
+from flask import current_app
+from flask_blog import create_app, db
+from flask_blog.config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
+
 
 class BaseTestCase(unittest.TestCase):
 
-    def create_app(self):
-        app = create_app(TestConfig)
-        return app
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_create_app(self):
+        self.assertFalse(current_app is None)
 
 if __name__ == '__main__':
     unittest.main()
